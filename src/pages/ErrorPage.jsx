f3c7d9568669c27795512fd49ac216eebe118d58
fxsym <fsyamsudin24@gmail.com>
2025-04-08 20:28:31 +0700

    Memperbaiki fungsi routing, dan menambahkan eror handling jika route tidak ditemukan

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,17 @@
 import { useRouteError } from "react-router-dom";
+import CenterContainer from "../layouts/CenterContainer";
 
 const ErrorPage = () => {
     const error = useRouteError();
 
     return (
-        <div>
-            <h1>Ooops!!</h1>
-            <p>Sorry, an expected error has occured</p>
-            <p>{error.statusText || error.message}</p>
-        </div>
+        <CenterContainer>
+            <div className="flex flex-col items-center">
+                <h1>Ooops!!</h1>
+                <p>Sorry, an expected error has occured</p>
+                <p>{error.statusText || error.message}</p>
+            </div>
+        </CenterContainer>
     )
 }
 
