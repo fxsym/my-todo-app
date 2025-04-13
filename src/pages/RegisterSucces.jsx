19714f0933b75ac85f66b5767421842fb80c1873
fxsym <fsyamsudin24@gmail.com>
2025-04-13 22:58:47 +0700

    Memperbarui tampilan success register

--- src/pages/RegisterSucces.jsx
+++ src/pages/RegisterSucces.jsx
@@ -10,7 +10,7 @@ export const RegisterSucces = () => {
                     <img src={AccountCreated} alt="" className="w-65"/>
                     <h1 className="text-3xl text-center">Your account has been created</h1>
                     <h1 className="text-sm text-center text-gray-500">Congratulations! Your account has been created now and your ready to acces your todo list from anywhere and everywhere</h1>
-                    <Link to="/login" className="cursor-pointer text-white hover:text-sky-800 transition-all bg-sky-500 p-3 rounded-xl hover:bg-sky-800 cursor-pointer w-full text-center">Login Now</Link>
+                    <Link to="/login" className="cursor-pointer text-white transition-all bg-sky-500 p-3 rounded-xl hover:bg-sky-800 cursor-pointer w-full text-center">Login Now</Link>
                 </div>
             </CenterContainer>
         </>
