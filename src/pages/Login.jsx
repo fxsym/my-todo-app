0ac065a2f33e5f3665a504c2751b5486e312f448
fxsym <fsyamsudin24@gmail.com>
2025-04-19 15:20:21 +0700

    Menambahkan responsive layar untuk halaman login dan register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { deviceInfo } from "../utils/loginUtils";
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from "../context/AuthContext";
 import { LoaderRing } from "../componenets/Loader";
+import LoginAnimation from "../assets/animations/LoginAnimation.webm"
 
 import AccountCreated from "../assets/images/AccountCreated.jpg"
 
@@ -35,15 +36,15 @@ export default function Login() {
         <>
             <CenterContainer>
                 <div className="flex items-center lg:justify-evenly lg:px-26 overflow-hidden">
-                    <div className="font-[Poppins] w-120 flex flex-col gap-6 lg:w-2/3 p-14 ">
-                            <h1 className="text-3xl font-bold lg:text-5xl">Login to your account </h1>
-                            <p className="text-sm text-gray-500 lg:text-xl">You must have an account first to be able to log in. Log in and save the activities you will do, access from anywhere and anytime.</p>
+                    <div className="font-[Poppins] w-120 flex flex-col gap-6 sm:w-160 lg:w-2/3 p-14 ">
+                            <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">Login to your account </h1>
+                            <p className="text-sm text-gray-500 sm:text-lg lg:text-xl">You must have an account first to be able to log in. Log in and save the activities you will do, access from anywhere and anytime.</p>
                         <div className="">
                             <form action="" className="flex flex-col items-center gap-2" onSubmit={handleSubmit}>
                                 <input type="text" placeholder="Insert your username" className="input-form" name="username" onChange={(e) => setUsername(e.target.value)} required />
                                 <input type="password" placeholder="Insert your password" className="input-form" name="password" onChange={(e) => setPassword(e.target.value)} required />
                                 {loading ? <LoaderRing /> : ""}
-                                <button className="bg-sky-500 py-4 w-full rounded-xl mt-4 text-white cursor-pointer hover:bg-sky-800 transition-all text-lg lg:text-xl lg:py-6">Login</button>
+                                <button className="bg-sky-500 py-4 w-full rounded-xl mt-4 text-white cursor-pointer hover:bg-sky-800 transition-all text-md sm:text-lg lg:text-xl">Login</button>
                                 {errorMsg && <p>{errorMsg}</p>}
                             </form>
                             <div className="flex justify-center items-center mt-8">
@@ -52,11 +53,12 @@ export default function Login() {
                                 <div className="w-[40%] h-[2px] bg-gray-500"></div>
                             </div>
                             <div className="flex justify-center mt-4">
-                                <p className="lg:text-lg">Don't have account ? <Link to="/register" className=" underline cursor-pointer text-sky-500 hover:text-sky-800 transition-all lg:text-lg">Create Account</Link></p>
+                                <p className="sm:text-md *:lg:text-lg">Don't have account ? <Link to="/register" className=" underline cursor-pointer text-sky-500 hover:text-sky-800 transition-all sm:text-md lg:text-lg">Create Account</Link></p>
                             </div>
                         </div>
                     </div>
                     <img src={AccountCreated} alt="" className="hidden lg:block w-65 sm:w-100 lg:w-1/2" />
+                    {/* <video src={LoginAnimation} alt="" className="hidden lg:block w-65 sm:w-100 lg:w-1/2" /> */}
                 </div>
             </CenterContainer>
         </>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,9 @@ import { LoaderRing } from "../componenets/Loader";
 import { Link, useNavigate } from "react-router-dom";
 import { checkUsername, getUsers, registerUser } from "../utils/userApi";
 
+import CreateAccount from "../assets/images/CreateAccount.png"
+
+
 export default function Register() {
     const [name, setName] = useState()
     const [username, setUsername] = useState()
@@ -48,31 +51,34 @@ export default function Register() {
     return (
         <>
             <CenterContainer>
-                <div className="font-[Poppins] w-[80%] flex flex-col gap-6">
-                    <h1 className="text-3xl">Create account </h1>
-                    <p className="text-xs text-gray-500">You must have an account first to be able to log in. Create your account and save the activities you will do, access from anywhere and anytime.</p>
-                    <div className="">
-                        <form action="" className="flex flex-col items-center gap-2" onSubmit={handleSubmit}>
-                            <input type="text" placeholder="| Name" className="input-form" name="name" onChange={(e) => setName(e.target.value)} required />
-                            <input type="text" placeholder="| Username" className="input-form" name="username" onChange={(e) => setUsername(e.target.value) } required />
-                            {username && (
-                                <p className={`text-sm ${takenUsername ? 'text-red-500' : 'text-green-500'}`}>
-                                    {takenUsername ? 'Username already exist' : ''}
-                                </p>
-                            )}
-                            <input type="email" placeholder="| Email" className="input-form" name="email" onChange={(e) => setEmail(e.target.value)} required />
-                            <input type="password" placeholder="| New password" className="input-form" name="password" onChange={(e) => setPassword(e.target.value)} required />
-                            {loading ? <LoaderRing /> : ""}
-                            <button className="bg-sky-500 py-4 w-[90%] rounded-xl mt-4 text-white cursor-pointer hover:bg-sky-800 transition-all text-lg">Create Account</button>
-                            {error && <p>{error}</p>}
-                        </form>
-                        <div className="flex justify-center items-center mt-8">
-                            <div className="w-[40%] h-[2px] bg-gray-500"></div>
-                            <p className="px-2 text-sm text-gray-500">Or</p>
-                            <div className="w-[40%] h-[2px] bg-gray-500"></div>
-                        </div>
-                        <div className="flex justify-center mt-4">
-                            <p className="">Already have account ? <Link to="/login" className=" underline cursor-pointer text-sky-500 hover:text-sky-800 transition-all">Login</Link></p>
+                <div className="flex items-center lg:justify-evenly lg:px-26 overflow-hidden">
+                    <img src={CreateAccount} alt="" className="hidden lg:block w-65 sm:w-100 lg:w-1/2" />
+                    <div className="font-[Poppins] w-120 flex flex-col gap-6 sm:w-160 lg:w-2/3 p-14 ">
+                        <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">Create account </h1>
+                        <p className="text-sm text-gray-500 sm:text-lg lg:text-xl">You must have an account first to be able to log in. Create your account and save the activities you will do, access from anywhere and anytime.</p>
+                        <div className="">
+                            <form action="" className="flex flex-col items-center gap-2" onSubmit={handleSubmit}>
+                                <input type="text" placeholder="| Name" className="input-form" name="name" onChange={(e) => setName(e.target.value)} required />
+                                <input type="text" placeholder="| Username" className="input-form" name="username" onChange={(e) => setUsername(e.target.value)} required />
+                                {username && (
+                                    <p className={`text-sm ${takenUsername ? 'text-red-500' : 'text-green-500'}`}>
+                                        {takenUsername ? 'Username already exist' : ''}
+                                    </p>
+                                )}
+                                <input type="email" placeholder="| Email" className="input-form" name="email" onChange={(e) => setEmail(e.target.value)} required />
+                                <input type="password" placeholder="| New password" className="input-form" name="password" onChange={(e) => setPassword(e.target.value)} required />
+                                {loading ? <LoaderRing /> : ""}
+                                <button className="bg-sky-500 py-4 w-full rounded-xl mt-4 text-white cursor-pointer hover:bg-sky-800 transition-all text-md sm:text-lg lg:text-xl">Create Account</button>
+                                {error && <p>{error}</p>}
+                            </form>
+                            <div className="flex justify-center items-center mt-8">
+                                <div className="w-[40%] h-[2px] bg-gray-500"></div>
+                                <p className="px-2 text-sm text-gray-500">Or</p>
+                                <div className="w-[40%] h-[2px] bg-gray-500"></div>
+                            </div>
+                            <div className="flex justify-center mt-4">
+                                <p className="sm:text-md *:lg:text-lg">Already have account ? <Link to="/login" className=" underline cursor-pointer text-sky-500 hover:text-sky-800 transition-all  sm:text-md lg:text-lg">Login</Link></p>
+                            </div>
                         </div>
                     </div>
                 </div>
