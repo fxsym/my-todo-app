408e0327a599f229cbb9e800f7f442fb40c24c20
fxsym <fsyamsudin24@gmail.com>
2025-06-19 22:48:27 +0700

    Redesign register succes page

--- src/pages/RegisterSucces.jsx
+++ src/pages/RegisterSucces.jsx
@@ -6,11 +6,15 @@ export const RegisterSucces = () => {
     return (
         <>
             <CenterContainer >
-                <div className="w-[90%] flex flex-col items-center justify-center gap-4">
-                    <img src={AccountCreated} alt="" className="w-65"/>
-                    <h1 className="text-3xl text-center">Your account has been created</h1>
-                    <h1 className="text-sm text-center text-gray-500">Congratulations! Your account has been created now and your ready to acces your todo list from anywhere and everywhere</h1>
-                    <Link to="/login" className="cursor-pointer text-white transition-all bg-sky-500 p-3 rounded-xl hover:bg-sky-800 cursor-pointer w-full text-center">Login Now</Link>
+                <div className="w-[90%] flex flex-col lg:flex-row items-center justify-center gap-4">
+                    <div className="lg:w-1/2 bg-amber-50 ">
+                        <img src={AccountCreated} alt="" className="w-full" />
+                    </div>
+                    <div className="flex flex-col items-center justify-center gap-4 lg:gap-6 lg:w-1/2">
+                        <h1 className="text-3xl text-center lg:text-4xl">Your account has been created</h1>
+                        <h1 className="text-sm text-center text-gray-500 md:text-lg lg:text-xl">Congratulations! Your account has been created now and your ready to acces your todo list from anywhere and everywhere</h1>
+                        <Link to="/login" className="cursor-pointer text-white transition-all bg-sky-500 p-3 rounded-xl hover:bg-sky-800 cursor-pointer w-full text-center">Login Now</Link>
+                    </div>
                 </div>
             </CenterContainer>
         </>
